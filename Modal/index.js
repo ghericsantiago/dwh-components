801e53cfd7714202718ddfaf71dbda8e2cabd9ce
Gheric Santiago <ghericsantiago@gmail.com>
2019-07-11 14:26:09 +0800

    Update Transpiled

--- Modal/index.js
+++ Modal/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+'use strict';
 
-const Modal = props => {
-    return (
-        <div>Modal</div>
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+require('./styles.css');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var MyComponent = function MyComponent() {
+    return _react2.default.createElement(
+        'h1',
+        null,
+        'Hello from My Component'
     );
-}
-
-export default Modal;
+};
+exports.default = MyComponent;
